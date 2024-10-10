84b165831b8d916137da36b85fb5423f47b35b81
emerson souza <emerson-santos1940@hotmail.com>
2024-10-09 22:06:48 -0300

    projeto Pizzaria
    Versão 3.0;
    Foram adicionadas imagens e corrigidas algumas seções e estilizações focado em responsividade além da inserção de alguns comandos em JavaScript

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,30 @@
+//Animações de escrever automaticamenteH1 e H2
+const texto = "O sabor da Margherita";
+const elementoTexto = document.getElementById("tituloAnimado");
+let index = 0;
+function digitar() {
+    if (index < texto.length) {
+        elementoTexto.innerHTML += texto.charAt(index);
+        index++;
+        setTimeout(digitar, 150); //Velocidade de digitação em ms;
+    }
+}
+//Animação H2
+const subtitle = "Há 20 anos na gastronomia Brasileira";
+const elementSubtitle = document.getElementById("subtituloAnimado");
+let valueSub = 0;
+function escrever() {
+    if (valueSub < subtitle.length) {
+        elementSubtitle.innerHTML += subtitle.charAt(valueSub);
+        valueSub++;
+        setTimeout(escrever, 150);
+    }
+}
+window.onload = function () {
+    digitar();
+    setTimeout(escrever, 3300);
+};
+
 /* Abre quando alguém clica no elemento span */
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
